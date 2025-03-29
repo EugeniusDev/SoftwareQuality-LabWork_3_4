616eb5678dc000f08b5a2ee28a476772e0ed259d
EugeniusDev <yevhenii.froliak@gmail.com>
2025-03-29 17:31:00 +0200

    Integer to roman tests implemented

---
+++ src/main/java/edu/froliak/Converter.java
@@ -0,0 +1,34 @@
+package edu.froliak;
+
+import java.util.List;
+
+/*
+  @author eugen
+  @project LabWork_3_4
+  @class Converter
+  @version 1.0.0
+  @since 3/29/2025 - 16.32
+*/public class Converter {
+    public static String convertToRoman(int number) {
+        if ((number <= 0) || (number > 4000)) {
+            throw new IllegalArgumentException(number + " is not in range (0,4000]");
+        }
+
+        List<RomanNumeral> romanNumerals = RomanNumeral.getReverseSortedValues();
+
+        int i = 0;
+        StringBuilder sb = new StringBuilder();
+
+        while ((number > 0) && (i < romanNumerals.size())) {
+            RomanNumeral currentSymbol = romanNumerals.get(i);
+            if (currentSymbol.getValue() <= number) {
+                sb.append(currentSymbol.name());
+                number -= currentSymbol.getValue();
+            } else {
+                i++;
+            }
+        }
+
+        return sb.toString();
+    }
+}
---
+++ src/main/java/edu/froliak/Main.java
@@ -0,0 +1,13 @@
+package edu.froliak;
+
+/*
+  @author eugen
+  @project Default (Template) Project
+  @class ${NAME}
+  @version 1.0.0
+  @since 3/29/2025 - 15.38
+*/public class Main {
+    public static void main(String[] args) {
+        System.out.println("Nothing interesting here bruh");
+    }
+}
